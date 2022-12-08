e58e677ed130367258084d251b5de3006950de57
Mor Mordechai Ben <mor6686@gmail.com>
2022-12-08 16:21:27 +0200

    test

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import { indexRoute } from './routes/index.router.js';
 // import { getAllUsers, findUser, addUser } from './utils.js';
 
 const app = express();
-const PORT = process.env.PORT | '8000';
+const PORT = process.env.PORT || '8000';
 console.log(process.env.PORT);
 
 app.use(express.json());
